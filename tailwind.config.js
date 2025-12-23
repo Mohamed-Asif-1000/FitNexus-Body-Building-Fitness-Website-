8504087181e2a72a4126b9c3a09d22216e7e8f21
Mohamed Asif.A <atmcreationsofficial@gmail.com>
2025-12-23 12:42:43 +0530

    Fixing path error version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*{html,js,ts}"],
+  content: [
+    "./index.html",         // Scans index.html in the root
+    "./src/**/*.{js,ts}",   // Scans all TS/JS files in your src folder
+    "./build/**/*.js"       // Scans compiled JS in build (if applicable)
+  ],
   theme: {
     extend: {
       fontFamily:{
-        heading:['"Russo One"','sans-serif'], // for Titles and Logos
-        content:['"Chakra Petch"','sans-serif'], // for Paragraph and Descriptions
+        heading:['"Russo One"','sans-serif'], 
+        content:['"Chakra Petch"','sans-serif'], 
       },
     },
   },
   plugins: [],
 }
-
